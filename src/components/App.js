2e43537fcdc955b683c3004ff7f8d868ca532e13
LifeDesign_MacBookD <lifedesign_macbookd@LifeDesign-MacBookDnoMacBook-Pro.local>
2020-07-16 16:19:33 +0900

    create event form and event table with bootstrap

---
+++ src/components/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App= () => {
+  return (
+    <div className="container-fluid">
+      <h4>イベント作成フォーム</h4>
+      <form>
+        <div className="form-group">
+          <label htmlfor="formEventTitle">タイトル</label>
+          <input className="form-control" id="formEventTitle" />
+        </div>
+
+        <div className="form-group">
+          <label htmlfor="formEventTitle">ボディー</label>
+          <textarea className="form-control" id="formEventTitle" />
+        </div>
+
+        <button className="btn btn-primary">イベント作成</button>
+        <button className="btn btn-danger">全てのイベント削除</button>
+      </form>
+
+      <h4>イベント一覧</h4>
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>タイトル</th>
+            <th>ボディー</th>
+            <th></th>
+          </tr>
+        </thead>
+      </table>
+    </div>
+  )
+}
+
+export default App;
