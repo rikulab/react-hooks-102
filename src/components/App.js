e7c1501718a4ad1daf952ceabbc4a7764d1179c0
LifeDesign_MacBookD <lifedesign_macbookd@LifeDesign-MacBookDnoMacBook-Pro.local>
2020-07-18 02:17:58 +0900

    display events, refactor and implement removew event button

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { useReducer, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Event from './Event';
 import reducer from './reducers';
 
 const App= () => {
@@ -50,6 +51,9 @@ const App= () => {
             <th></th>
           </tr>
         </thead>
+        <tbody>
+          { state.map((event, index) => (<Event key={index} event={event} />))}
+        </tbody>
       </table>
     </div>
   )
