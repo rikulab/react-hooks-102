21dfa6d792180a2b4deac3e566efe3f20eeb384e
LifeDesign_MacBookD <lifedesign_macbookd@LifeDesign-MacBookDnoMacBook-Pro.local>
2020-07-17 16:49:38 +0900

    implement event creation

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,42 @@
-import React from 'react';
+import React, { useReducer, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import reducer from './reducers';
 
 const App= () => {
+  const [state, dispatch] = useReducer(reducer, [])
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+
+  const addEvent = e => {
+    e.preventDefault()
+    
+    dispatch({
+      type: 'CREATE_EVENT',
+      title,
+      body
+    })
+
+    setTitle('')
+    setBody('')
+  }
+
+  console.log({state})
+
   return (
     <div className="container-fluid">
       <h4>イベント作成フォーム</h4>
       <form>
         <div className="form-group">
           <label htmlfor="formEventTitle">タイトル</label>
-          <input className="form-control" id="formEventTitle" />
+          <input className="form-control" id="formEventTitle" value={title} onChange={e => setTitle(e.target.value)} />
         </div>
 
         <div className="form-group">
           <label htmlfor="formEventTitle">ボディー</label>
-          <textarea className="form-control" id="formEventTitle" />
+          <textarea className="form-control" id="formEventTitle" value={body} onChange={e => setBody(e.target.value)} />
         </div>
 
-        <button className="btn btn-primary">イベント作成</button>
+        <button className="btn btn-primary" onClick={addEvent}>イベント作成</button>
         <button className="btn btn-danger">全てのイベント削除</button>
       </form>
 
