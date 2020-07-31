b7b5a081a86344e5c3c355e97b46b9c003a777dc
LifeDesign_MacBookD <lifedesign_macbookd@LifeDesign-MacBookDnoMacBook-Pro.local>
2020-07-31 14:27:02 +0900

    replace type names with constants

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,21 @@
-import React, { useReducer } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import EventForm from './EventForm';
-import Events from './Events';
-import reducer from './reducers';
+import React, { useReducer } from 'react'
 
-const App= () => {
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import EventForm from './EventForm'
+
+import Events from './Events'
+import reducer from '../reducers'
+
+const App = () => {
   const [state, dispatch] = useReducer(reducer, [])
 
   return (
     <div className="container-fluid">
       <EventForm state={state} dispatch={dispatch} />
       <Events state={state} dispatch={dispatch} />
-      
     </div>
   )
 }
 
-export default App;
+export default App
